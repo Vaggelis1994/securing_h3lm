c5f6244681d0b3f42afc2ae35a47d77118883657
Evangelos <e.daravigkas@ihu.edu.gr>
2020-01-05 23:46:33 +0100

    final steps; pending TODOs, testing

--- secure_helm_misc.sh
+++ secure_helm_misc.sh
@@ -6,31 +6,54 @@
 
 function secure_roll_deployments_automatically {
 
-    #check status
-    #find deployments $1
-    #grep -nrw $1 -e "sha256sum"
-    #if not deployed -> sed or input line checksum/config
+    '''
+        Often times _ConfigMaps_ or _Secrets_ are injected as configuration files in containers or 
+        there are other external dependencies changes that require rolling pods. Depending on 
+        the application a restart may be required should those be updated with a subsequent 
+        `helm upgrade`, but if the deployment spec itself didn’t change the application keeps 
+        running with the old configuration resulting in an inconsistent deployment.
 
-    grep "kind: Deployment"
-    grep "kind: ConfigMap"
-    grep "kind: Secrets"
+    '''
 
-    secrets=$(grep "checksum/secrets" | wc -l)
-    secrets_checksum=$(grep "checksum/secrets" | grep "sha256sum" | wc -l)
-    if [ $secrets_used ] && [ $secrets -ne $secrets_checksum ]; then
-        sed
+    # TODO: check status
+    # TODO: retrieve file
+    # TODO: kubectl 
+
+    file=""
+
+    # TODO: use this as counter 
+    # deployment=$(grep "Deployment:" $file | wc -l)
+
+    configs=$(grep "checksum/config" $file | wc -l)
+    configs_checksum=$(grep "checksum/config" $file | grep "sha256sum" | wc -l) 
+
+    # check if the sha256sum is already used for all of the checksum/config entries
+    if [ $configs_used ] && [ $configs -ne $configs_checksum ]; then
+        line=$(grep "checksum/config" $file | cut -d '}' -f 1)
+        checksum_line=$(echo $line" | sha256sum }}") 
+        sed -i 's/$line/$checksum_line/g' $file
     fi
 
-    configs=$(grep "checksum/config" | wc -l)
-    configs_checksum=$(grep "checksum/config" | grep "sha256sum" | wc -l) 
-    if [ $configs_used ] && [ $configs -ne $configs_checksum ]; then
-        sed
+    # inject rollme entity to always roll the deployment
+    rollme=$(grep "rollme" $file | wc -l)
+    if [ $rollme -ne 0 ]; then
+        sed -i 's/annotations:/annotations:\n
+                rollme: {{ randAlphaNum 5 | quote }}/g'
     fi
 }
 
 function secure_image_pull_secrets {
 
-    # https://github.com/helm/helm/blob/master/docs/charts_tips_and_tricks.md#creating-image-pull-secrets
+    '''
+    
+        Source: https://helm.sh/docs/howto/charts_tips_and_tricks/#creating-image-pull-secrets
+    
+    '''
+
+    # 1. Assume that the credentials are defined in the "values.yaml" file.
+
+    # 2. Definition of helper template 
+
     touch "_imagePullSecrets.yaml"
 
     echo "{{- define \"imagePullSecret\" }}" > "_imagePullSecrets.yaml"
@@ -38,9 +61,25 @@ function secure_image_pull_secrets {
         (printf \"%s:%s\" .Values.imageCredentials.username .Values.imageCredentials.password \
         | b64enc) | b64enc }}" >> "_imagePullSecrets.yaml"
     echo "{{- end }}" >> "_imagePullSecrets.yaml"
+
+    # 3. Using the helper template in a larger template to create the Secret manifest
+
+    touch "secret_manifest.yaml"
+
+    echo "apiVersion: v1\
+          kind: Secret\
+          metadata: \
+            name: myregistrykey \
+            type: kubernetes.io/dockerconfigjson \
+        data:
+            .dockerconfigjson: {{ template \"imagePullSecret\" . }}" > secret_manifest.yaml
+
+    # kubectl create -f secret_manifest.yaml
 }
 
 function secure_generate_aliases {
+
+    # TODO: To review all the options and their respective secure versions
 
     alias secure_helm_delete="helm delete --tls"
     alias secure_helm_dependency_build="helm dependency build --verify"
@@ -64,4 +103,5 @@ function secure_generate_aliases {
     alias secure_helm_upgrade="helm upgrade --recreate-pods --tls --verify --key-file"
     alias secure_helm_verify="helm verify"
     alias secure_helm_version="helm version --tls"
+
 }
